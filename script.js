99b26ea5eee261fe9903ab54c8295db2d83d4abd
BigsizeJ <apacjessie@gmail.com>
2022-05-26 20:43:45 +0800

    Finished

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const BOARDSIZE = document.querySelector(".boardsize")
 const CLEAR = document.querySelector(".clear")
 const COLOR = document.querySelector(".color")
 const RANDOM = document.querySelector(".random")
-
-
+const ERASER = document.querySelector(".eraser")
+const COLOR_MODE = document.querySelector(".colorMode")
+const BTN = document.querySelectorAll(".btn")
 let currentColor = "#000"
+let test = null
 
 function createBoard(arg1 = 16, arg2 = 16){
     CONTAINER.style.gridTemplateColumns = `repeat(${arg1}, 1fr)`
@@ -37,19 +39,41 @@ function clearBoard(){
 }
 
 function changeColor(){
-    currentColor = COLOR.value
+    clearInterval(test)
+    BTN.forEach(btn => btn.classList.remove("active"))
+
+    if(this.name == "color"){
+        this.classList.add("active")
+        currentColor = COLOR.value
+    }
+    if(this.name == "change"){
+        COLOR_MODE.classList.add("active")
+        currentColor = this.value
+    }
+    else if(this.name == "eraser"){
+        this.classList.add("active")
+        currentColor = "#fefefe"
+    }
 }
 
 
 function rainbowMode(){
-
+    BTN.forEach(btn => btn.classList.remove("active"))
+    test = setInterval(() => {
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        this.classList.add("active") 
+        currentColor = `rgb(${r}, ${g}, ${b})`
+    })
 }
 
 createBoard()
-
 
 
 BOARDSIZE.addEventListener("change", changeBoard)
 CLEAR.addEventListener("click", clearBoard)
 RANDOM.addEventListener("click", rainbowMode)
 COLOR.addEventListener("change", changeColor)
+ERASER.addEventListener("click", changeColor)
+COLOR_MODE.addEventListener("click", changeColor)
